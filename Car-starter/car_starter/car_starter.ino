a26fb043df9dbef7edc6c6283044ffd3f30f8e6f
Guillermo Rivera <guillo5@mac.com>
2019-06-08 19:33:27 -0400

    New functions
    With some combined proceses

--- Car-starter/car_starter/car_starter.ino
+++ Car-starter/car_starter/car_starter.ino
@@ -3,6 +3,7 @@ int lock= 4;
 int trunk= 7;
 int ignition= 8;
 int panic= 12;
+int standard_time= 250;
 
 void setup()
 {
@@ -16,64 +17,101 @@ pinMode(panic, OUTPUT); //Panic button on FOB
 void loop()
 {
 Unlock();
-Lock();
-Trunk();
-Ignition();
+Open_Trunk();
+Close_Trunk();
+Start_Car();
 Panic();
 }
 
-void Unlock()
+void Unlock() //function for unlock 2 clicks
 {
 digitalWrite(unlock, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(unlock, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(unlock, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(unlock, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 }
 
-void Lock()
+void Lock() //function for lock 2 clicks
 {
 digitalWrite(lock, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(lock, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(lock, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(lock, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 }
 
-void Trunk()
+void Trunk() //function for trunk 2 clicks
 {
 digitalWrite(trunk, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(trunk, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(trunk, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(trunk, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 }
 
-void Ignition()
+void Ignition() //function for ignition 2 clicks
 {
 digitalWrite(ignition, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(ignition, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(ignition, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(ignition, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 }
 
-void Panic()
+void Panic() //function for panic 1 click
 {
 digitalWrite(panic, HIGH);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
 digitalWrite(panic, LOW);
-delay(250); // Wait for 250 millisecond(s)
+delay(standard_time); // Wait for standard_time millisecond(s)
+}
+
+//Open Trunk 
+//------------------------------------------------------------------
+/*Logic: 
+ * First we must lock the car doors and then open the trunk door
+ */
+//------------------------------------------------------------------
+
+void Open_Trunk()
+{
+Lock();
+Trunk();
+}
+
+//Close Trunk 
+//------------------------------------------------------------------
+/*Logic: 
+ * Press the trunk button again. This function will only run if trunk 
+ * is actually open
+ */
+//------------------------------------------------------------------
+void Close_Trunk()
+{
+Trunk();
+}
+
+//Start Car
+//------------------------------------------------------------------
+/*Logic: 
+ * First we must lock the car doors and then Press the car ignition
+ */
+//------------------------------------------------------------------
+void Start_Car()
+{
+Lock();
+Ignition();
 }
